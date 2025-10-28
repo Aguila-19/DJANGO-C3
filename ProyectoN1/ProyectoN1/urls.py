da0311f75a445e19ce4a793de39853bb764d7825
Aguila-19 <ar3757800@gmail.com>
2025-10-28 12:09:19 -0600

    Agregar estructura inicial del proyecto Django con configuración básica, vistas y plantillas.

---
+++ ProyectoN1/ProyectoN1/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from miapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.saludo),
+    path('info/', views.info),
+]
